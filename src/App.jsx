49ba25281faef6720b271dd3af0a561aabc907c7
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-12-04 17:46:24 +0100

    add content input

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [articlesList, setArticleList] = useState([]);
-  const [newArticleTitle, setNewArticleTitle] = useState("");
+  const [articleFields, setArticleFields] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleArticleForm = (e) => {
+    setArticleFields({
+      ...articleFields,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handlePostSubmit = (e) => {
     e.preventDefault();
 
     const newArticle = {
-      title: newArticleTitle,
+      title: articleFields.title,
+      content: articleFields.content,
     };
 
     const updatedArticlesList = [...articlesList, newArticle];
 
     setArticleList(updatedArticlesList);
+  };
 
+  useEffect(() => {
     console.log(articlesList);
-  };
-
-  const handleNewTitle = (e) => {
-    setNewArticleTitle(e.target.value);
-  };
+  }, [articlesList]);
 
   return (
     <>
@@ -30,13 +39,29 @@ function App() {
       <main>
         <div>
           <form onSubmit={handlePostSubmit}>
-            <label htmlFor="post-title">Inserisci il titolo del post</label>
-            <input
-              onChange={handleNewTitle}
-              value={newArticleTitle}
-              type="text"
-              id="post-title"
-            />
+            <div>
+              {/* input titolo articolo */}
+              <label htmlFor="post-title">Inserisci il titolo del post</label>
+              <input
+                onChange={handleArticleForm}
+                value={articleFields.title}
+                type="text"
+                id="post-title"
+                name="title"
+              />
+            </div>
+            <div>
+              {/* input contenuto articolo */}
+              <label htmlFor="post-content">Inserisci contenuto</label>
+              <input
+                onChange={handleArticleForm}
+                value={articleFields.content}
+                type="text"
+                id="post-content"
+                name="content"
+              />
+            </div>
+            {/* bottone submit */}
             <button>Crea</button>
           </form>
         </div>
@@ -47,6 +72,7 @@ function App() {
               return (
                 <div key={article.title}>
                   <h4>{article.title}</h4>
+                  <p>{article.content}</p>
                 </div>
               );
             })}
